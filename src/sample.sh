fd120e247ec50740142fffe310936221db863249
h-akira <79441679+h-akira@users.noreply.github.com>
2023-02-25 02:18:03 +0900

    fix

--- src/sample.sh
+++ src/sample.sh
@@ -14,4 +14,4 @@ ${BASE}/src/rename_windows_screenshots.py ${DIR_A}/*
 ${BASE}/src/rename_windows_capture.py ${DIR_B}/*
 ${BASE}/src/upload2photo.py ${DIR_A}/* -t ${TOKEN} -c ${CREDENTIALS} -l ${LOG} -s ${STATE} --no-stdout
 ${BASE}/src/upload2photo.py ${DIR_B}/* -t ${TOKEN} -c ${CREDENTIALS} -l ${LOG} -s ${STATE} --no-stdout
-echo "Photo: `cat ${BASE}/secret/state.txt`"
+echo "Photo: `cat ${STATE}`"
